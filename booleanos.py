664d79cac3fb5ecd007f903b664ac5c786ef396e
Arthur Bertoluci <arthurbertoluci@icloud.com>
2025-01-28 20:32:26 -0300

    Booleanos

---
+++ booleanos.py
@@ -0,0 +1,29 @@
+# Condição Verdadeira
+True
+# Condição Falsa
+False
+
+if True:
+    print("Bloco IF sera executado")
+else:
+    print("Bloco ELSE não sera executado")
+  
+# Operadores Logicos AND
+if True and True:
+    print("Bloco sera executado")
+    
+if True and False:
+    print("Bloco não sera executado")
+    
+if False and False:
+    print("Bloco não sera executado")
+    
+# Operadores Logicos OR
+if True or False:
+    print("Bloco sera executado")
+
+if False or False:
+    print("Bloco não sera executado")
+
+if True or True:
+    print("Bloco sera executado")
