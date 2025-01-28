5047610d7fea0fdb4835cdafb5e443a95045f42c
Arthur Bertoluci <arthurbertoluci@icloud.com>
2025-01-28 20:31:56 -0300

    Métodos de Numeros

---
+++ numeros.py
@@ -0,0 +1,19 @@
+# Soma +
+soma = 10 + 10
+#Subtração -
+subtracao = 18 - 6
+# Divisão /
+divisao = 15 / 3
+# Divisão inteiro
+divisao_inteiro = 15 // 3
+# Multiplicação *
+multiplicacao = 2 * 9
+# Potenciação **
+potenciacao = 2 ** 10
+
+# Modulo (Resto da divisão)
+modulo = 5 % 2
+
+# Print dos resultados
+
+print(soma, subtracao, divisao, divisao_inteiro, multiplicacao, potenciacao, modulo)
